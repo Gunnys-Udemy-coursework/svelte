1370e59c15847e439da01a5a41af739b9d7f94b2
RMKendall <ryan.morris.kendall@gmail.com>
2021-02-25 00:24:32 -0600

    Complete Section 2 example work

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,25 @@
 <script>
+    import ContactCard from "./ContactCard.svelte";
+
 	export let name;
 	export let age;
+	let title = "";
+	let description = "";
+	let image;
+
+	$: uppercaseName = name.toUpperCase();
 
 	function incrementAge(){
 	    age += 1;
+    }
+
+    function changeName(){
+	    name = 'That Guy';
+    }
+
+    function nameInput(event){
+	    const enteredValue = event.target.value;
+	    name = enteredValue;
     }
 </script>
 
@@ -13,5 +29,20 @@
 	}
 </style>
 
-<h1>Hello {name}, my age is {age}!</h1>
+<h1>Hello {uppercaseName}, my age is {age}!</h1>
 <button on:click={incrementAge}>Change Age</button>
+<!--<button on:click={changeName}>Change Name</button>-->
+<!--<input type="text" value={name} on:input={nameInput}>-->
+<input type="text" bind:value={name}/>
+<input type="text" bind:value={title}/>
+<input type="text" bind:value={image}/>
+<textarea rows="3" bind:value={description}></textarea>
+
+<!--Notice {description} is not paired. This is an allowed shortcut thru Svelte because the local variable and associated-->
+<!--variable have the same name.-->
+<ContactCard
+        userName="{name}"
+        jobTitle="{title}"
+        {description}
+        userImage="{image}"
+/>
